ab3db08da30037c720b721a98bfef279dff5d15f
JyotsnaReddy26 <sjreddy2627@gmail.com>
2026-05-29 14:49:41 +0530

    Time: 7 ms (30.84%), Space: 19.3 MB (56.38%) - LeetHub

---
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        result=[]
+        for i in range(len(nums)):
+            s=0
+            while nums[i]>0:
+
+                s+=nums[i]%10
+                nums[i]//=10
+            result.append(s)
+        return min(result)
