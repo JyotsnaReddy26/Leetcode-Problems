f011f6ca07a214c43413cd9495929c45baa55bfa
JyotsnaReddy26 <sjreddy2627@gmail.com>
2026-06-20 09:58:22 +0530

    Time: 60 ms (5.97%), Space: 36.1 MB (15.5%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,3 +1,12 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return len(nums)!= len(set(nums))
+        dicto={}
+        for i in range(len(nums)):
+            if nums[i] in dicto:
+                dicto[nums[i]]+=1
+            else:
+                dicto[nums[i]]=1
+        for key,value in dicto.items():
+            if value>1:
+                return True
+        return False
