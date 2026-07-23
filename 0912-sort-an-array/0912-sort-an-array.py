f35a52cc592b31148bf4a30a0b95f2e849c35673
JyotsnaReddy26 <sjreddy2627@gmail.com>
2026-07-23 09:51:08 +0530

    Time: 26 ms (87.68%), Space: 27.7 MB (50.2%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,7 +1,3 @@
-class Solution(object):
-    def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        return sorted(nums)
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+       return sorted(nums)
